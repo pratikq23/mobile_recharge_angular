7c48b9e5c2a289b159a42a393466dffa85683b3f
pratik <pratikq23@gmail.com>
2017-09-10 22:14:49 +0530

    added service files

--- src/app/adminlogin/admin-login.module.ts
+++ src/app/adminlogin/admin-login.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AdminLoginRoutingModule } from './admin-login-routing.module';
 import { AdminLoginComponent } from './admin-login.component';
-import { LoginService } from './login.service';
-import { RoleService } from './role.service';
+import { LoginService } from '../services/login.service';
+import { RoleService } from '../services/role.service';
 
 
 @NgModule({
     imports: [
         CommonModule,
-        AdminLoginRoutingModule
+        AdminLoginRoutingModule,
+        FormsModule
     ],
     declarations: [AdminLoginComponent],
       providers: [
--- src/app/layout/signup/signup-routing.module.ts
+++ src/app/layout/signup/signup-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup.component';
 
+
 const routes: Routes = [
     { path: '', component: SignupComponent }
 ];
